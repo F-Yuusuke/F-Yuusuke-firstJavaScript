27fd145f41f19eb177a9d5d56eee76734e2a981d
Ｆ.Ｙuusuke <>
2020-03-29 21:48:42 +0900

    html cssはコピー jsはaddEventListenerの使い方を調べながらidを取得してそのidにイベントがあったらなんの処理をするのかコードに書いた

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
 // これからコードを書きます
+// let res = document.getElementById('btn');
+// let result = document.getElementById('result');
+// btn.addEventListener('click',function(){
+//   let res = ['大吉',　'中吉',　'小吉',　'末吉',　'凶'];
+//   let randomNum = Math.random();
+
+//   let num = Math.floor
+//   (randomNum * res.length);
+
+//   console.log(res[num]);
+//     result.textContent = (res[num]);
+// });
+
+// const res = document.getElementById('btn');
+// console.log(res);
+// const result = document.getElementById('result');
+// btn.addEventListener()
+
+const res = document.getElementById('btn');
+console.log(res);
+btn.addEventListener('mousedown',function(){
+    console.log("成功したよ！");
+});
