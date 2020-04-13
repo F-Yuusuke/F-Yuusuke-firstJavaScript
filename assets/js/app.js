0a29895ebc2798891b74ff019d40365be6546461
Ｆ.Ｙuusuke <>
2020-04-13 18:31:49 +0900

    画像を追加 スライドしたら画像がフワッと出てくるアニメーションのコードをそれぞれ書いた

--- assets/js/app.js
+++ assets/js/app.js
@@ -135,6 +135,34 @@ document.querySelector('#main').innerHTML = `
 
 
 
+// ふわっと現れる画像アニメーション できなかった
+// 到達したら要素を表示させる
+function showElememtAnimation() {
+    let element = document.getElementsByClassName('js-animation');
+    // 要素がなかったら処理をキャンセル
+    if(!element) return;
+
+    let showTiming = window.innerHeight > 768 ? 200 : 40; //要素が出てくるタイミングはここで調節
+    let scrollY = window.pageY0ffset;
+    let windowH = window.innerHeight;
+    
+    for(let i = 0; i < element.length; i++) {
+        let elemClientRect = element[i].getBoundingClientRect(); let elemY = scrollY + elemClientRect.top; if(scrollY + windowH - showTiming > elemY) {
+            element[i].classList.add('is-show');
+        } else if(scrollY + windowH < elemY) {
+            // 上にスクロールして再度非表示にする場合はこちらを記述
+            element[i].classList.remove('is-show');
+        }
+    }
+}
+showElememtAnimation();
+window.addEventListener('scroll', showElememtAnimation);
+
+
+
+
+
+
 
 
 
